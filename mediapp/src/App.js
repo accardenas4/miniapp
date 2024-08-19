a1118220d6028f315b57599e2589a7be98a66fc4
ANA CRISTINA CARDENAS CABRERA <accardenas4@utpl.edu.ec>
2024-08-19 17:49:44 -0500

    Fix 1

--- mediapp/src/App.js
+++ mediapp/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Formulario from './components/Formulario/Formulario';
 import Servicios from './components/Servicios/Servicios';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importar Bootstrap
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Servicios />} />
-        <Route path="/servicios" element={<Formulario />} />
-        {/* Agrega otras rutas aquí si es necesario */}
+        <Route path="/" element={<Formulario />} />
+        <Route path="/servicios" element={<Servicios />} />
       </Routes>
     </Router>
   );
